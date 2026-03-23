633e892f6a2c47ec1d31561b4498c4d2891634ab
monly <annv@heligate.com.vn>
2026-03-23 16:38:37 +0700

    feat: Initialize React application with Bootstrap and React Query
    - Add main application structure in App.tsx
    - Create Header component with theme toggle
    - Implement TabsContent component for user and blog management
    - Develop BlogsTable component for displaying and managing blog entries
    - Create UsersTable component for displaying and managing user entries
    - Add modal components for creating, editing, and deleting blogs and users
    - Implement pagination for user management
    - Set up API calls for user and blog data management
    - Configure TypeScript with appropriate settings
    - Set up Vite for development environment
    - Include Toast notifications for user feedback

---
+++ src/components/users.table.tsx
@@ -0,0 +1,162 @@
+import Table from 'react-bootstrap/Table';
+import { useState, forwardRef } from 'react';
+import Button from 'react-bootstrap/Button';
+import UserCreateModal from './modal/user.create.modal';
+import UserEditModal from './modal/user.edit.modal';
+import UserDeleteModal from './modal/user.delete.modal';
+import UsersPagination from './pagination/users.pagination';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Popover from 'react-bootstrap/Popover';
+import { useQuery, keepPreviousData } from '@tanstack/react-query';
+
+interface IUser {
+    id: number;
+    name: string;
+    email: string;
+}
+
+const PopoverComponent = forwardRef((props: any, ref: any) => {
+    const { id } = props;
+
+    const { isPending, isError, data: user, error } = useQuery<IUser>({
+        queryKey: ['todos', id],
+        queryFn: () => fetch(`http://localhost:8000/users/${id}`).then(res => res.json()),
+    })
+
+    if (isPending) {
+        return <span>Loading...</span>
+    } else if (isError) {
+        return <span>Error: {error.message}</span>
+    }
+    return (
+        <Popover ref={ref} {...props}>
+            <Popover.Header as="h3">Detail User</Popover.Header>
+            <Popover.Body>
+                <div>ID = {user?.id}</div>
+                <div>Name = {user?.name}</div>
+                <div>Email = {user?.email}</div>
+            </Popover.Body>
+        </Popover>
+    )
+})
+
+function UsersTable() {
+
+    const [isOpenCreateModal, setIsOpenCreateModal] = useState<boolean>(false);
+
+    const [isOpenUpdateModal, setIsOpenUpdateModal] = useState<boolean>(false);
+    const [dataUser, setDataUser] = useState({});
+
+    const [isOpenDeleteModal, setIsOpenDeleteModal] = useState<boolean>(false);
+
+    const [currentPage, setCurrentPage] = useState<number>(1);
+    const [totalPages, setTotalPages] = useState<number>(1);
+
+    const PAGE_SIZE = 20;
+
+
+    const handleEditUser = (user: any) => {
+        setDataUser(user);
+        setIsOpenUpdateModal(true);
+    }
+
+    const handleDelete = (user: any) => {
+        setDataUser(user);
+        setIsOpenDeleteModal(true);
+    }
+
+    const { isPending, isError, data: users, error } = useQuery<IUser[]>({
+        queryKey: ['todos', currentPage],
+        queryFn: () => fetch(`http://localhost:8000/users?_page=${currentPage}&_limit=${PAGE_SIZE}`).then(res => {
+            const total_items = res.headers.get('X-Total-Count') ?? 0;
+            const total_pages = Math.ceil(Number(total_items) / PAGE_SIZE);
+            setTotalPages(total_pages);
+            return res.json();
+        }),
+        placeholderData: keepPreviousData,
+    })
+
+    if (isPending) {
+        return <span>Loading...</span>
+    }
+
+    if (isError) {
+        return <span>Error: {error.message}</span>
+    }
+
+
+    return (
+        <>
+            <div style={{ display: "flex", justifyContent: "space-between", margin: "15px 0" }}>
+                <h4>Table Users</h4>
+                <Button variant="primary"
+                    onClick={() => setIsOpenCreateModal(true)}
+                >Add New</Button>
+            </div>
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {users?.map(user => {
+                        return (
+                            <tr key={user.id}>
+                                <OverlayTrigger trigger="click" placement="right"
+                                    rootClose
+                                    overlay={<PopoverComponent id={user.id} />}
+                                >
+                                    <td><a href='#' onClick={(e) => e.preventDefault()}>{user.id}</a></td>
+                                </OverlayTrigger>
+
+                                <td>{user.name}</td>
+                                <td>{user.email}</td>
+                                <td>
+                                    <Button
+                                        variant="warning"
+                                        onClick={() => handleEditUser(user)}
+                                    >
+                                        Edit
+                                    </Button>&nbsp;&nbsp;&nbsp;
+                                    <Button
+                                        variant="danger"
+                                        onClick={() => handleDelete(user)}
+                                    >
+                                        Delete
+                                    </Button>
+                                </td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </Table>
+            <UsersPagination
+                totalPages={totalPages}
+                currentPage={currentPage}
+                setCurrentPage={setCurrentPage}
+            />
+            <UserCreateModal
+                isOpenCreateModal={isOpenCreateModal}
+                setIsOpenCreateModal={setIsOpenCreateModal}
+            />
+
+            <UserEditModal
+                isOpenUpdateModal={isOpenUpdateModal}
+                setIsOpenUpdateModal={setIsOpenUpdateModal}
+                dataUser={dataUser}
+            />
+
+            <UserDeleteModal
+                dataUser={dataUser}
+                isOpenDeleteModal={isOpenDeleteModal}
+                setIsOpenDeleteModal={setIsOpenDeleteModal}
+            />
+        </>
+    );
+}
+
+export default UsersTable;
